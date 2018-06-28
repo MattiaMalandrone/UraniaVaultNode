92529df6c1eafa08631b08a515db83dcd15950ec
Mattia Malandrone <mattia.malandrone@live.com>
2018-06-28 12:39:01 +0200

    fix creazione lista albi

--- routes/albi.js
+++ routes/albi.js
@@ -1,58 +1,110 @@
 const _ = require('lodash');
 const { Albo } = require('../models/albo');
+const { User } = require('../models/user');
 const { Relation } = require('../models/relation');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/list', async (req, res) => {
+router.get('/list/:email/:countAlbiLoaded', async (req, res) => {
 
-    const userId = mongoose.Types.ObjectId("5b2a67073762f42e3c51d1b5");
+    const user = await User.findOne({ email: req.params.email });
 
+    const countAlbiLoaded = parseInt(req.params.countAlbiLoaded);
+
+    console.log(countAlbiLoaded);
+
+    /**
+     * When a $sort immediately precedes a $limit in the pipeline,
+     * the $sort operation only maintains the top n results as it progresses,
+     * where n is the specified limit,
+     * and MongoDB only needs to store n items in memory.
+     * This optimization still applies when allowDiskUse is true and the n items exceed the aggregation memory limit.
+     */
     const albi = await Albo.aggregate([
         {
-            $lookup: {
-                from: "relations",
-                localField: "_id",
-                foreignField: "alboId",
-                as: "status"
+            $lookup: { from: "relations", localField: "_id", foreignField: "alboId", as: "status" }
+        },
+        {
+            $match: {
+                numero : {
+                    $gt:  countAlbiLoaded
+                }
             }
         },
         {
-            $project:
-            {
+            $sort : { numero : 1 }
+        },
+        {
+            $limit: 10
+        },
+        {
+            $project: {
                 numero: 1,
                 title: 1,
-                status:
-                {
-                    $filter:
-                    {
-                        input: "$status",
-                        as: "rel",
-                        cond: { $eq: [ "$$rel.userId", userId ] }
-                    }
-                }
+                status: { $filter: { input: "$status", as: "rel", cond: { $eq: [ "$$rel.userId", user._id ] } } }
             },
         },
-        // { "$unwind": "$status" } // usare questo, però prima devo creare un unit sulla relation per auopoppolarla
-    ]).limit(10);
-
-    console.log(albi)
+        // { "$unwind": "$status" } // usare questo, però prima devo creare un init sulla relation per auopoppolarla
+    ]);
+    //.sort('numero');
+    //.limit(10);
 
     res.send(albi);
 });
 
 router.post('/init', async (req, res) => {
-    const { error } = validate(req.body);
 
-    if (error)
+    if (!req.body.email)
       return res.status(400).send(error.details[0].message);
 
     let user = await User.findOne({ email: req.body.email });
 
-    console.log(user._id);
+    const albi = await Albo.find();
+
+    let insertBulk = [];
+
+    for (var i = 0, len = albi.length; i < len; i++) {
+        const relation = new Relation({
+            alboId: albi[i]._id,
+            userId: user._id,
+            status: 0
+        });
+        insertBulk.push(relation);
+    }
+
+    Relation.insertMany(insertBulk);
 
     res.send();
+});
+
+router.post('/update', async (req, res) => {
+    if (!req.body.email)
+      return res.status(400).send(error.details[0].message);
+
+    const ultimoAlboCatalogo = await Albo.findOne({}).sort('-numero');
+    const ultimoAlboUserExists = await Relation.count({ alboId: ultimoAlboCatalogo._id }) > 0;
+
+    console.log(ultimoAlboCatalogo);
+    console.log(ultimoAlboUserExists);
+
+    if(ultimoAlboUserExists)
+        return res.send();
+
+    Relation.create(ultimoAlboCatalogo);
+
+    return res.send();
+});
+
+router.get('/isFirstAccess/:email', async (req, res) => {
+    let user = await User.findOne({ email: req.params.email });
+    const count = await Relation.count({userId : user._id});
+    return res.send({count: count});
+});
+
+router.delete('/deleteCollectionAlbi', async (req, res) => {
+    await Relation.remove();
+    return res.send();
 });
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -6,28 +6,39 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signin', async (req, res) => {
-  console.log(req.body);
+
+  console.log('signin...');
+
   const { error } = validate(req.body);
+
+  console.log(`ERROR signin`);
 
   if (error)
     return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
 
+  console.log(user);
+
   if (!user)
     return res.status(400).send('Invalid email or password.');
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  console.log(validPassword);
 
   if (!validPassword)
     return res.status(400).send('Invalid email or password.');
 
   const token = user.generateAuthToken();
 
+  console.log(token);
+
   res.send({token: token});
 });
 
 function validate(req) {
+  console.log(req);
   const schema = {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
